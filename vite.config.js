f5688333502dfeb15243a8ac867c3ae7019099f9
alices_x <1277787672@qq.com>
2022-08-09 15:15:46 +0800

    3.8.3

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig(({ mode, command }) => {
                 // https://cn.vitejs.dev/config/#server-proxy
                 '/dev-api': {
                     // target: 'http://192.168.123.102:8080/',
-                    target: 'http://192.168.123.96:8080/',
+                    target: 'http://api.sp.shanhaiping.com',
                     // target: 'http://api.sp.shanhaiping.com',
                     changeOrigin: true,
                     rewrite: (p) => p.replace(/^\/dev-api/, '')
