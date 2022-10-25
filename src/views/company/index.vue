9ae5775d8145fb16923db8326a2c6a7074a0a1ee
wangmeixi <wangmeixi@shanhaiping.com>
2022-10-25 15:33:49 +0800

    修改

--- src/views/company/index.vue
+++ src/views/company/index.vue
@@ -6,34 +6,37 @@
         <el-button v-if="inputType!=='readey'" type="primary" @click="handelRead('sure')">确定</el-button>
         <el-button v-if="inputType!=='readey'" @click="handelRead('cancel')">取消</el-button>
       </el-affix>
-  </div>
+    </div>
     <div class="form-box">
       <el-form ref="formRef" :rules="rules" :model="form" label-width="105px">
         <el-form-item label="企业logo" prop="logo">
           <div>
             <el-upload
-              class="back-box"
-              :disabled="inputType==='readey'"
-              :action="uploadData.uploadUrl"
-              :headers="{'Authorization':uploadData.token}"
-              :data="uploadData.imgBannerParam"
-              method:="POST"
-              accept="image/*"
-              :limit="1"
-              :file-list="fileList"
-              :show-file-list="false"
-              :on-success="handleAvatarSuccess"
-              :before-upload="beforeAvatarUpload">
+                class="back-box"
+                :disabled="inputType==='readey'"
+                :action="uploadData.uploadUrl"
+                :headers="{'Authorization':uploadData.token}"
+                :data="uploadData.imgBannerParam"
+                method:="POST"
+                accept="image/*"
+                :limit="1"
+                :file-list="fileList"
+                :show-file-list="false"
+                :on-success="handleAvatarSuccess"
+                :before-upload="beforeAvatarUpload">
               <div class="demo-image__placeholder">
                 <div class="block">
-                  <el-image :src="form.logo" v-if="form.logo !== null && form.logo !== ''" style="width: 500px;height: 180px;border: 1px solid var(--el-border-color);border-radius: 4px; overflow: hidden">
+                  <el-image :src="form.logo" v-if="form.logo !== null && form.logo !== ''"
+                            style="width: 500px;height: 180px;border: 1px solid var(--el-border-color);border-radius: 4px; overflow: hidden">
                     <template #placeholder>
                       <div class="image-slot">Loading<span class="dot">...</span></div>
                     </template>
                   </el-image>
-                  <el-image v-if="form.logo == null || form.logo == '' || form.logo == undefined" style="width: 500px;height: 180px;border: 1px solid var(--el-border-color);border-radius: 4px; overflow: hidden">
+                  <el-image v-if="form.logo == null || form.logo == '' || form.logo == undefined"
+                            style="width: 500px;height: 180px;border: 1px solid var(--el-border-color);border-radius: 4px; overflow: hidden">
                     <template #error>
-                      <div class="image-slot" style="display: flex;justify-content:center;align-items:center;height: 180px">
+                      <div class="image-slot"
+                           style="display: flex;justify-content:center;align-items:center;height: 180px">
                         <el-icon :size="48">
                           <Picture/>
                         </el-icon>
@@ -43,45 +46,47 @@
                 </div>
               </div>
               <div :class="inputType==='readey'? 'hide-back' : 'show-back'">点击上传 / 替换logo</div>
-              </el-upload>
+            </el-upload>
             <div class="form-tips">推荐尺寸500*180</div>
           </div>
         </el-form-item>
         <el-form-item label="企微ID" prop="plainCorpId">
-          <el-input v-model="form.plainCorpId" :disabled="inputType==='readey'" />
+          <el-input v-model="form.plainCorpId" :disabled="inputType==='readey'"/>
         </el-form-item>
         <el-form-item label="法人代表" prop="legalPersonId">
-          <el-select v-model="form.legalPersonId" :disabled="inputType==='readey'" placeholder=" " style="width: 500px;">
-            <el-option v-for="item in legalPersonList" :key="item.userId" :label="item.userName" :value="item.userId" />
+          <el-select v-model="form.legalPersonId" :disabled="inputType==='readey'" placeholder=" "
+                     style="width: 500px;">
+            <el-option v-for="item in legalPersonList" :key="item.userId" :label="item.userName" :value="item.userId"/>
           </el-select>
         </el-form-item>
         <el-form-item label="企业简称" prop="name">
-          <el-input v-model="form.name" :disabled="inputType==='readey'" />
+          <el-input v-model="form.name" :disabled="inputType==='readey'"/>
         </el-form-item>
         <el-form-item label="企业全称" prop="fullname">
-          <el-input v-model="form.fullname" :disabled="inputType==='readey'" />
+          <el-input v-model="form.fullname" :disabled="inputType==='readey'"/>
         </el-form-item>
         <el-form-item label="企业类型" prop="corpType">
-          <el-select v-model="form.corpType" :disabled="inputType==='readey'" placeholder=" " filterable style="width: 500px;">
-            <el-option v-for="item in corpTypeList" :key="item.value" :label="item.label" :value="item.value" />
+          <el-select v-model="form.corpType" :disabled="inputType==='readey'" placeholder=" " filterable
+                     style="width: 500px;">
+            <el-option v-for="item in corpTypeList" :key="item.value" :label="item.label" :value="item.value"/>
           </el-select>
         </el-form-item>
         <el-form-item label="门店编号" v-if="form.corpType == 4">
           <el-input v-model="form.code"
                     clearable
                     :disabled="inputType==='readey'"
-                    />
+          />
         </el-form-item>
         <el-form-item label="发票抬头">
-          <el-input v-model="form.invoiceTitle" :disabled="inputType==='readey'" />
+          <el-input v-model="form.invoiceTitle" :disabled="inputType==='readey'"/>
         </el-form-item>
         <el-form-item label="企业电话">
-          <el-input v-model="form.phone" clearable :disabled="inputType==='readey'" />
+          <el-input v-model="form.phone" clearable :disabled="inputType==='readey'"/>
         </el-form-item>
         <el-form-item label="详细地址">
           <el-input v-model="form.address" clearable :disabled="inputType==='readey'"></el-input>
         </el-form-item>
-        <el-divider />
+        <el-divider/>
         <el-form-item label="成员数量">
           <span>{{ form.userCount }}个成员</span>
         </el-form-item>
@@ -91,7 +96,7 @@
         <el-form-item label="员工规模">
           <span>{{ form.userCount }}/{{ form.corpUserMax }}</span>
         </el-form-item>
-        <el-divider />
+        <el-divider/>
         <el-form-item label="企业ID">
           <span>{{ corpId }}</span>
         </el-form-item>
@@ -117,7 +122,7 @@ import {ref, reactive} from "vue";
 import {useStore} from 'vuex';
 import {getToken} from '../../utils/auth';
 import {getCorpInfo, getCorpEdit} from '../../api/company/info.js'
-import { listUser } from "../../api/system/user";
+import {listUser} from "../../api/system/user";
 import useUserStore from "../../store/modules/user";
 import {ElMessage} from "element-plus";
 
@@ -162,13 +167,14 @@ const corpTypeList = [
 ]
 
 const rules = reactive({
-  legalPersonId: [{ required: true, message: '请选择法人代表', trigger: 'change' }],
-  name: [{ required: true, message: '请输入企业简称', trigger: 'blur' }],
+  legalPersonId: [{required: true, message: '请选择法人代表', trigger: 'change'}],
+  name: [{required: true, message: '请输入企业简称', trigger: 'blur'}],
   // fullname: [{ required: true, message: '请输入企业全称', trigger: 'blur' }],
-  corpType: [{ required: true, message: '请选择企业类型', trigger: 'change' }]
+  corpType: [{required: true, message: '请选择企业类型', trigger: 'change'}]
 })
 // 法定代表人
 const legalPersonList = ref([])
+
 // 法定代表人数据
 function getSelectData() {
   listUser().then(res => {
@@ -186,6 +192,7 @@ function handleAvatarSuccess(res) {
     ElMessage.error(res.msg)
   }
 }
+
 // 上传验证
 function beforeAvatarUpload(file) {
   const isPicture = file.type === 'image/jpeg' || file.type === 'image/png'
@@ -199,10 +206,11 @@ function beforeAvatarUpload(file) {
   }
   return true
 }
+
 // 上传状态
-function handleChange (file, fileList) {
+function handleChange(file, fileList) {
   if (fileList.length > 1) {
-    fileList.splice(0,1)
+    fileList.splice(0, 1)
   }
 }
 
@@ -213,17 +221,19 @@ function getFormData() {
     }
   })
 }
+
 // 点击事件
 function handelRead(type) {
-  if (type === 'edit'){
-    inputType.value='write'
+  if (type === 'edit') {
+    inputType.value = 'write'
   } else if (type === 'sure') {
     onSubmit()
   } else if (type === 'cancel') {
-    inputType.value='readey'
+    inputType.value = 'readey'
     getFormData()
   }
 }
+
 async function onSubmit() {
   let v = await proxy.$refs["formRef"].validate();
   if (v) {
@@ -236,6 +246,7 @@ async function onSubmit() {
     })
   }
 }
+
 getSelectData()
 getFormData()
 </script>
@@ -246,35 +257,42 @@ getFormData()
     text-align: end;
     height: 0;
   }
+
   .form-box {
     .el-form-item {
       max-width: 680px;
       margin: 0 auto 18px;
     }
+
     .form-tips {
       font-size: 12px;
       color: #787878;
       margin-top: 7px;
     }
+
     .el-input {
       width: 500px;
     }
+
     :deep(.el-form-item--default) .el-form-item__label {
       color: #606266;
       font-weight: 600;
     }
+
     .back-box {
       position: relative;
+
       .hide-back {
         display: none;
       }
+
       .show-back {
         width: 100%;
         height: 180px;
         text-align: center;
         line-height: 180px;
         color: #fff;
-        background: rgba(0,0,0,.5);
+        background: rgba(0, 0, 0, .5);
         position: absolute;
         left: 0;
         top: 0;
